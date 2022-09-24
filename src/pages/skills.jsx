bda5f1c9fa953b8c3bef5cbd58ee3b7c3f4053eb
Isaque de Sousa <isaque.error404@gmail.com>
2022-09-24 01:58:05 -0300

    feat(skills): create new skills file

---
+++ src/pages/skills.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@chakra-ui/react";
+
+export default function Skills() {
+  return (
+    <>
+      <Container>
+        <h1>Em construção</h1>
+      </Container>
+    </>
+  );
+}
