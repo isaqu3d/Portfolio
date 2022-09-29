6413dec9cee78097b1314097eba9851ed2426eca
Isaque de Sousa <isaque.error404@gmail.com>
2022-09-29 13:17:43 -0300

    feat(skillscard): create new component and add in pages skills

---
+++ src/components/SkillsCard.jsx
@@ -0,0 +1,23 @@
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import React from "react";
+
+export default function SkillsCard({ children }) {
+  return (
+    <Box
+      border="2px"
+      borderColor={useColorModeValue("teal.300", "purple.700")}
+      display="flex"
+      justifyContent="center"
+      px={4}
+      py={[2, 2, 0]}
+      opacity={[1, 1, 0.4]}
+      borderRadius={4}
+      _hover={{
+        borderColor: useColorModeValue("teal.400", "pink.400"),
+        opacity: 1,
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
