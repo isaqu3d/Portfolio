e965c7b31b836ce0596093d94dd17ea5f559073b
Isaque de Sousa <isaque.error404@gmail.com>
2022-09-29 13:17:01 -0300

    feat(skills-tools): create utils and add script

---
+++ src/utils/skills-tools.js
@@ -0,0 +1,51 @@
+import {
+  SiGithub,
+  SiInsomnia,
+  SiLinux,
+  SiNotion,
+  SiPostman,
+  SiVercel,
+  SiVisualstudiocode,
+} from "react-icons/si";
+
+export const SKILLS_TOOLS = [
+  {
+    id: 1,
+    icon: SiVisualstudiocode,
+    color: "#23A7F2",
+  },
+
+  {
+    id: 2,
+    icon: SiVercel,
+  },
+
+  {
+    id: 3,
+    icon: SiLinux,
+    color: "#000",
+  },
+
+  {
+    id: 4,
+    icon: SiInsomnia,
+    color: "#4d00c8",
+  },
+
+  {
+    id: 5,
+    icon: SiPostman,
+    color: "#fb7505",
+  },
+  {
+    id: 6,
+    icon: SiGithub,
+    color: "#000",
+  },
+
+  {
+    id: 7,
+    icon: SiNotion,
+    color: "#000",
+  },
+];
