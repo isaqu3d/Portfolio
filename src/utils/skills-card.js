9e97a3f6cfb172ec4bac2188c96fd26f4ed5b5e1
Isaque de Sousa <isaque.error404@gmail.com>
2022-09-29 13:14:19 -0300

    feat(skills-card): create utils and add script

---
+++ src/utils/skills-card.js
@@ -0,0 +1,71 @@
+import {
+  SiChakraui,
+  SiCss3,
+  SiGit,
+  SiHtml5,
+  SiJavascript,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiReact,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+
+export const SKILLS_CARD = [
+  {
+    id: "1",
+    icon: SiHtml5,
+    color: "#f06529",
+  },
+  {
+    id: "2",
+    icon: SiCss3,
+    color: "#2965f1",
+  },
+  {
+    id: "3",
+    icon: SiJavascript,
+    color: "#F0DB4F",
+  },
+  {
+    id: "4",
+    icon: SiReact,
+    color: "#61dafb",
+  },
+  {
+    id: "5",
+    icon: SiNextdotjs,
+    color: "#000000",
+  },
+  {
+    id: "6",
+    icon: SiTypescript,
+    color: "#007acc",
+  },
+  {
+    id: "7",
+    icon: SiNodedotjs,
+    color: "#3c873a",
+  },
+  {
+    id: "8",
+    icon: SiTailwindcss,
+    color: "#38BDF8",
+  },
+  {
+    id: "9",
+    icon: SiStyledcomponents,
+    color: "#dea336",
+  },
+  {
+    id: "10",
+    icon: SiChakraui,
+    color: "#46C8C1",
+  },
+  {
+    id: "11",
+    icon: SiGit,
+    color: "#f34f29",
+  },
+];
