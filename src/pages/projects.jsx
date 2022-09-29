d73a24d617c7daf0e555a67218a4d5d15e293fa2
Isaque de Sousa <isaque.error404@gmail.com>
2022-09-29 17:24:47 -0300

    feat(projects): add component projectGridItem and new components

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,10 +1,42 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
+import thumbEventPlaform from "../../public/project/eventplatoform.png";
+import thumbPomodoro from "../../public/project/move.it.png";
+import ProjectGridItem from "../components/Projects/ProjectGridItem ";
+
+import Section from "../components/Section";
 
 export default function projects() {
   return (
     <>
       <Container>
-        <h1>Em construção</h1>
+        <Heading as="h3" variant="section-title" fontSize={20} mb={4}>
+          Projetos
+        </Heading>
+
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <ProjectGridItem
+              id="eventplatform"
+              title="Plataforma de Evento"
+              thumbnail={thumbEventPlaform}
+            >
+              Projeto criado na NLW com intuito de fazer uma plataforma de
+              aulas. Como por exemplo: Aulas de programação, Matemática,
+              intensivão de aulas, etc.
+            </ProjectGridItem>
+          </Section>
+
+          <Section>
+            <ProjectGridItem
+              title="Pomodoro"
+              thumbnail={thumbPomodoro}
+              id="move.it"
+            >
+              Projeto de Pomodoro criado na NLW com intuito de
+              melhorar/organizar seus estudos e tempo.
+            </ProjectGridItem>
+          </Section>
+        </SimpleGrid>
       </Container>
     </>
   );
