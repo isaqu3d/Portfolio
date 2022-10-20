8fa273b9fc0102c62a90e46146ab74b202d3ed75
isaque.s <isaque.error404@gmail.com>
2022-10-20 15:20:39 -0300

    refactor(badge): rename bagde

--- src/components/Projects/BagdeColor.jsx
+++
@@ -1,9 +0,0 @@
-import { Badge } from "@chakra-ui/react";
-
-export default function BadgeMeta({ children }) {
-  return (
-    <Badge colorScheme="green" mr={2}>
-      {children}
-    </Badge>
-  );
-}
