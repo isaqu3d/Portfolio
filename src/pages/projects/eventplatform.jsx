45c0477b1f49a589f68dc438ff8fbd5765523554
isaque.s <isaque.error404@gmail.com>
2022-10-20 15:25:04 -0300

    refactor: add new `component`create

--- src/pages/projects/eventplatform.jsx
+++ src/pages/projects/eventplatform.jsx
@@ -1,57 +1,35 @@
-import {
-  Badge,
-  Container,
-  Link,
-  List,
-  ListItem,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+import { ListItemProject } from "../../components/Projects/ListItemProject";
 
-import { Paragraph } from "../../components/Paragraph";
-import { default as BadgeMeta } from "../../components/Projects/BagdeColor";
+import HeaderItemProject from "../../components/Projects/HeaderItemProject";
 import ProjectImage from "../../components/Projects/ProjectImage";
-import Title from "../../components/Projects/Title";
+import Section from "../../components/Section";
 
 export default function eventplatform() {
   return (
     <Container>
-      <Title>
-        Plataforma de Evento
-        <Badge
-          ml={2}
-          color={useColorModeValue("whiteAlpha.800", "whiteAlpha.700")}
-          bg={useColorModeValue("teal.600", "gray.300")}
-        >
-          2022
-        </Badge>
-      </Title>
+      <Section>
+        <HeaderItemProject
+          title="Plataforma de Evento"
+          badgeYear={2022}
+          description=" Seja bem-vindo ao projeto criado na NLW com intuito de fazer uma
+          plataforma de aulas. Nesta plataforma vocês pode cadastrar
+          aulas/vídeos. Como por exemplo: Aulas de programação, intensivão de
+          aulas, Receitas de comidas, etc."
+        />
 
-      <Paragraph>
-        Seja bem-vindo ao projeto criado na NLW com intuito de fazer uma
-        plataforma de aulas. Nesta plataforma vocês pode cadastrar aulas/vídeos.
-        Como por exemplo: Aulas de programação, intensivão de aulas, Receitas de
-        comidas, etc.
-      </Paragraph>
+        <ListItemProject
+          href="https://event-platform-livid.vercel.app/"
+          url="https://event-platform-livid.vercel.app/"
+          hrefGitHub="https://github.com/X-SpeedBlack-X/event-platform"
+          urlGitHub="https://github.com/X-SpeedBlack-X/event-platform"
+          description="ReactJS, Graphql, TailwindCSS, Router, Apollo, date-fns."
+          target="_blank"
+        />
 
-      <List ml={4} my={4}>
-        <ListItem>
-          <BadgeMeta>Website</BadgeMeta>
-          <Link
-            href="https://event-platform-livid.vercel.app/"
-            target="_blank"
-            color={useColorModeValue("teal.500", "pink.300")}
-          >
-            https://event-platform-livid.vercel.app/
-          </Link>
-        </ListItem>
-
-        <ListItem>
-          <BadgeMeta>Tech</BadgeMeta>
-          <span>ReactJS, Graphql, TailwindCSS, Router, Apollo, date-fns.</span>
-        </ListItem>
-      </List>
-      <ProjectImage src="/project/eventplatform_2.png" />
-      <ProjectImage src="/project/eventplatform_3.png" />
+        <ProjectImage src="/project/eventplatform_2.png" />
+        <ProjectImage src="/project/eventplatform_3.png" />
+      </Section>
     </Container>
   );
 }
--- src/pages/projects/move.it.jsx
+++ src/pages/projects/move.it.jsx
@@ -1,58 +1,36 @@
-import {
-  Badge,
-  Container,
-  Link,
-  List,
-  ListItem,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+import { ListItemProject } from "../../components/Projects/ListItemProject";
 
-import { Paragraph } from "../../components/Paragraph";
-import { default as BadgeMeta } from "../../components/Projects/BagdeColor";
+import HeaderItemProject from "../../components/Projects/HeaderItemProject";
 import ProjectImage from "../../components/Projects/ProjectImage";
-import Title from "../../components/Projects/Title";
+import Section from "../../components/Section";
 
 export default function eventplatform() {
   return (
     <Container>
-      <Title>
-        Move.It - Pomodoro
-        <Badge
-          ml={2}
-          color={useColorModeValue("whiteAlpha.800", "whiteAlpha.700")}
-          bg={useColorModeValue("teal.600", "gray.300")}
-        >
-          2021
-        </Badge>
-      </Title>
+      <Section>
+        <HeaderItemProject
+          title="Move.It - Pomodoro"
+          badgeYear={2021}
+          description=" Seja bem-vindo ao projeto Move.it. Esse projeto é um aplicação de um
+          pomodoro para melhorar seus estudos, pois, quando o tempo chega ao fim
+          e lança um desafio para você fazer. E depois de completar o desafio
+          você ganha XP para upar de level, e podendo iniciar o Pomodoro
+          novamente."
+        />
 
-      <Paragraph>
-        Seja bem-vindo ao projeto Move.it. Esse projeto é um aplicação de um
-        pomodoro para melhorar seus estudos, pois, quando o tempo chega ao fim e
-        lança um desafio para você fazer. E depois de completar o desafio você
-        ganha XP para upar de level, e podendo iniciar o Pomodoro novamente.
-      </Paragraph>
-
-      <List ml={4} my={4}>
-        <ListItem>
-          <BadgeMeta>Website</BadgeMeta>
-          <Link
-            href="https://nlw-moveit-six.vercel.app/"
-            target="_blank"
-            color={useColorModeValue("teal.500", "pink.300")}
-          >
-            https://nlw-moveit-six.vercel.app/
-          </Link>
-        </ListItem>
-
-        <ListItem>
-          <BadgeMeta>Tech</BadgeMeta>
-          <span>NextJS, TypeScript.</span>
-        </ListItem>
-      </List>
-      <ProjectImage src="/project/pomodoro_2.png" />
-      <ProjectImage src="/project/pomodoro_3.png" />
-      <ProjectImage src="/project/pomodoro_4.png" />
+        <ListItemProject
+          url="https://nlw-moveit-six.vercel.app/"
+          href="https://nlw-moveit-six.vercel.app/"
+          hrefGitHub="https://github.com/X-SpeedBlack-X/nlw-move.it"
+          urlGitHub="https://github.com/X-SpeedBlack-X/nlw-move.it"
+          description="NextJS, TypeScript"
+          target="_blank"
+        />
+        <ProjectImage src="/project/pomodoro_2.png" />
+        <ProjectImage src="/project/pomodoro_3.png" />
+        <ProjectImage src="/project/pomodoro_4.png" />
+      </Section>
     </Container>
   );
 }
