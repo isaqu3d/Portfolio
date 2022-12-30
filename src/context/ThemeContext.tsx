4a5f469982bfb30046e207dd70d09a7e4a1dcae6
isaque.s <isaque.error404@gmail.com>
2022-12-30 15:45:44 -0300

    feat(context): create new context ThemeProvider

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useState } from "react";
+
+type ThemeContextProps = {
+  theme: string;
+  changeTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextProps>({} as ThemeContextProps);
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("");
+
+  function changeTheme() {
+    const newTheme = theme === "" ? "dark" : "";
+
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  }
+
+  useEffect(() => {
+    const saveTheme = localStorage.getItem("theme");
+    setTheme(saveTheme);
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme, changeTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export default ThemeContext;
