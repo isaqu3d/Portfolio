7f8d4eed00353a02fa9ac4b253516cd191a7b1e6
isaque.s <isaque.error404@gmail.com>
2023-01-02 17:36:18 -0300

    refactor: change jsx to tsx and rename file name

---
+++ src/components/CardTypeWriter.tsx
@@ -0,0 +1,19 @@
+import useThemeData from "../hook/useThemeData";
+import { Typewriter } from "./TypeWriter";
+
+export function CardTypeWriter() {
+  const { theme } = useThemeData();
+  return (
+    <>
+      <div
+        className={`${
+          theme
+            ? "bg-white-100 bg-opacity-40  text-black"
+            : "bg-gray-600 text-white-300"
+        }  flex justify-center items-center  shadow-xl rounded-lg mb-4 p-1 text-center h-12`}
+      >
+        <Typewriter />
+      </div>
+    </>
+  );
+}
