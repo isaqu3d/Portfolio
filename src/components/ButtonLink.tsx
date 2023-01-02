bd06f0a9569706f5ead4449ce3047fcea4e2aadf
isaque.s <isaque.error404@gmail.com>
2023-01-02 16:28:42 -0300

    refactor: rename button to ButtonLink and add tag a

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -3,19 +3,21 @@ import useThemeData from "../hook/useThemeData";
 type ButtonProps = {
   children: string;
   className?: string;
+  href: string;
 };
 
-export function Button({ children, className }: ButtonProps) {
+export function ButtonLink({ children, className, href }: ButtonProps) {
   const { theme } = useThemeData();
   return (
-    <button
+    <a
+      href={href}
       className={`${
         theme
           ? " bg-teal-400 hover:bg-teal-600 text-white-100 "
           : " bg-teal-200 hover:bg-teal-400 text-black"
-      } ${className}transition-colors  font-medium p-2 px-4 rounded-md`}
+      } ${className} flex items-center transition-colors gap-2 font-medium py-2 px-4 rounded-md`}
     >
       {children}
-    </button>
+    </a>
   );
 }
