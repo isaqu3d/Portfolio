9c2aab17de19849eeeea5c29b996727da2c1c6cf
isaque.s <isaque.error404@gmail.com>
2023-01-02 15:37:15 -0300

    refactor: change chakra-ui to tailwindcss

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,53 +1,55 @@
-import { Button, Heading, Link, List, ListItem } from "@chakra-ui/react";
+import { default as NextLink } from "next/link";
 
 import { IoLogoGithub, IoLogoInstagram, IoLogoLinkedin } from "react-icons/io5";
+import useThemeData from "../hook/useThemeData";
+import { Heading } from "./Heading";
 
-export default function SocialMedia() {
+const LinkItem = ({ href, children, ...props }) => {
+  /*   const router = useRouter();
+  const active = router.asPath === href; */
+  const { theme } = useThemeData();
+
+  return (
+    <NextLink href={href} passHref scroll={false}>
+      <a
+        href={href}
+        className={`${
+          theme
+            ? "text-teal-700 hover:bg-teal-400/20 w-36 hover:underline hover:underline-offset-[6px] hover:decoration-1 hover:decoration-teal-600"
+            : "text-teal-200 hover:bg-teal-400/10 w-36 hover:underline hover:underline-offset-[6px] hover:decoration-1 hover:decoration-pink-500"
+        } flex items-center rounded-md p-2 ml-2 gap-2 w-36 font-semibold`}
+        {...props}
+      >
+        {children}
+      </a>
+    </NextLink>
+  );
+};
+
+export function SocialMedia() {
   return (
     <>
-      <Heading as="h3" variant="section-title">
-        Conecte-se Comigo
-      </Heading>
-      <List>
-        <ListItem>
-          <Link href="https://www.instagram.com/_isaque.s_/" target="_blank">
-            <Button
-              variant="ghost"
-              colorScheme="teal"
-              leftIcon={<IoLogoInstagram />}
-            >
-              @_isaque.s_
-            </Button>
-          </Link>
-        </ListItem>
+      <Heading>Conecte-se comigo</Heading>
 
-        <ListItem>
-          <Link
-            href="https://www.linkedin.com/in/isaque-de-sousa/"
-            target="_blank"
-          >
-            <Button
-              variant="ghost"
-              colorScheme="teal"
-              leftIcon={<IoLogoLinkedin />}
-            >
-              @linkedln
-            </Button>
-          </Link>
-        </ListItem>
+      <div className="font-serif">
+        <LinkItem href="https://www.instagram.com/_isaque.s_/" target="_blank">
+          <IoLogoInstagram />
+          @_isaque.s_
+        </LinkItem>
 
-        <ListItem>
-          <Link href="https://github.com/X-SpeedBlack-X" target="_blank">
-            <Button
-              variant="ghost"
-              colorScheme="teal"
-              leftIcon={<IoLogoGithub />}
-            >
-              @GitHub
-            </Button>
-          </Link>
-        </ListItem>
-      </List>
+        <LinkItem
+          href="https://www.linkedin.com/in/isaque-de-sousa/"
+          target="_blank"
+        >
+          <IoLogoLinkedin />
+          @linkedin
+        </LinkItem>
+
+        <LinkItem href="https://github.com/X-SpeedBlack-X" target="_blank">
+          <IoLogoGithub />
+          @GitHub
+        </LinkItem>
+      </div>
     </>
   );
 }
