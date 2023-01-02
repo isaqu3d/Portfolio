ceeba77305a9f4667783c34c8208f4527d2d5b1c
isaque.s <isaque.error404@gmail.com>
2023-01-02 11:24:33 -0300

    refactor(logo): change Chakra-UI to tailwindCSS

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,40 +1,20 @@
-import { Text, useColorModeValue } from "@chakra-ui/react";
-import styled from "@emotion/styled";
 import Link from "next/link";
 import { RiCodeSSlashLine } from "react-icons/ri";
-
-const LogoBox = styled.span`
-  font-weight: bold;
-  font-size: 18px;
-  display: inline-flex;
-  align-items: center;
-  height: 30px;
-  line-height: 20px;
-  padding: 10px;
-  > svg {
-    transition: 200ms ease;
-  }
-  &:hover > svg {
-    transform: rotate(20deg);
-  }
-`;
+import useThemeData from "../hook/useThemeData";
 
 export default function Logo() {
+  const { theme } = useThemeData();
   return (
     <Link href="/">
       <a>
-        <LogoBox>
-          <RiCodeSSlashLine size={20} />
-          <Text
-            color={useColorModeValue("gray.800", "whiteAlpha.900")}
-            fontFamily='M PLUS Rounded 1c", sans-serif'
-            ml={3}
-            variant="page-title"
-            as="h2"
-          >
-            Isaque de Sousa
-          </Text>
-        </LogoBox>
+        <div
+          className={`${
+            theme ? " text-gray-600 " : "text-white-100"
+          } inline-flex text-lg font-sans font-semibold items-center h-8 leading-5 p-3 group tracking-tighter`}
+        >
+          <RiCodeSSlashLine className="group-hover:rotate-[30deg] transition-all" />
+          <h2 className="">Isaque de Sousa</h2>
+        </div>
       </a>
     </Link>
   );
