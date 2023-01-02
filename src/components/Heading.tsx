824cf5a6a9d2828bc3d1db1efcf221e88b6229e9
isaque.s <isaque.error404@gmail.com>
2023-01-02 11:27:19 -0300

    feat(heading): create new component in tailwindcss

---
+++ src/components/Heading.tsx
@@ -0,0 +1,19 @@
+import useThemeData from "../hook/useThemeData";
+
+type HeadingProps = {
+  children: string;
+  className?: string;
+};
+
+export function Heading({ children, className }: HeadingProps) {
+  const { theme } = useThemeData();
+  return (
+    <h1
+      className={`${
+        theme ? "text-gray-600" : "text-white-100"
+      } text-xl font-bold underline underline-offset-[6px] decoration-4 decoration-gray-300 mb-4 mt-3 ${className}`}
+    >
+      {children}
+    </h1>
+  );
+}
