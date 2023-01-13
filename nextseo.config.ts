6dd02f03b3d23bb881d0dab15e6f857c49af88a7
isaque.s <isaque.error404@gmail.com>
2023-01-13 12:16:33 -0300

    feat: create config nextseo

---
+++ nextseo.config.ts
@@ -0,0 +1,9 @@
+import { NextSeoProps } from "next-seo";
+
+const seo: NextSeoProps = {
+  title: "Portfolio - Isaque",
+  description:
+    "Desenvolvedor front-end, entusiasta da tecnologia e um apaixonado por programação.",
+};
+
+export default seo;
