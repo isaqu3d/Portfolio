51f60da7432033e9a2204fe45c8eae987a11a877
Isaque de Sousa <isaque.error404@gmail.com>
2023-01-24 10:23:41 -0300

    feat: add compoments Container

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,10 +1,10 @@
-import { SkillsCard } from "../components/SkillsCard";
-
-import GitHubCalendar from "react-github-calendar";
-import { Heading } from "../components/Heading";
-
 import { NextSeo } from "next-seo";
+import GitHubCalendar from "react-github-calendar";
+import { Container } from "../components/Container";
+
+import { Heading } from "../components/Heading";
 import { Motion } from "../components/Motion";
+import { SkillsCard } from "../components/SkillsCard";
 import { SKILLS_CARD } from "../utils/skills-card";
 import { SKILLS_TOOLS } from "../utils/skills-tools";
 
@@ -17,51 +17,56 @@ function SkillCardRender({ icon: Icon, color }) {
 }
 
 export default function Skills() {
+  /*   const [showModal, setShowModal] = useState<string | undefined>(); */
+
   return (
-    <Motion>
+    <>
       <NextSeo
         title="Habilidades - Isaque de Sousa"
         description="Pagina das minhas habilidades e ferramentas que eu utilizo no meu dia a dia"
       />
-      <div className="max-w-lg px-4">
-        <Heading>Habilidades</Heading>
+      <Container>
+        <Motion>
+          <Heading>Habilidades</Heading>
 
-        <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
-          {SKILLS_CARD.map((skills) => (
-            <SkillCardRender
-              icon={skills.icon}
-              key={skills.id}
-              color={skills.color}
+          <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
+            {SKILLS_CARD.map((skills) => (
+              <SkillCardRender
+                icon={skills.icon}
+                key={skills.id}
+                color={skills.color}
+              />
+            ))}
+
+            {/*  {SKILLS_CARD.find((item) => item?.id === showModal?.id) ? (
+              <ModalContent item={showModal} />
+            ) : null} */}
+          </div>
+
+          <Heading>Ferramentas</Heading>
+          <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
+            {SKILLS_TOOLS.map((skills) => (
+              <SkillCardRender
+                icon={skills.icon}
+                key={skills.id}
+                color={skills.color}
+              />
+            ))}
+          </div>
+          <Heading>Commits</Heading>
+          <div>
+            <GitHubCalendar
+              username="x-speedblack-x"
+              year={new Date().getFullYear()}
+              color="#cd5ff8"
+              hideColorLegend
+              fontSize={18}
+              blockSize={14}
+              blockMargin={5}
             />
-          ))}
-        </div>
-
-        <Heading>Ferramentas</Heading>
-
-        <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
-          {SKILLS_TOOLS.map((skills) => (
-            <SkillCardRender
-              icon={skills.icon}
-              key={skills.id}
-              color={skills.color}
-            />
-          ))}
-        </div>
-
-        <Heading>Commits</Heading>
-
-        <div>
-          <GitHubCalendar
-            username="x-speedblack-x"
-            year={new Date().getFullYear()}
-            color="#cd5ff8"
-            hideColorLegend
-            fontSize={18}
-            blockSize={14}
-            blockMargin={5}
-          />
-        </div>
-      </div>
-    </Motion>
+          </div>
+        </Motion>
+      </Container>
+    </>
   );
 }
