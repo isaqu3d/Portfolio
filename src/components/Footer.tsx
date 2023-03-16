77ebc8315e3ee7e0789dd50a8aaac12656ccf167
Error404 <isaque.error404@gmail.com>
2023-03-16 11:40:00 -0300

    refactor: change classname

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,6 @@
 export default function Footer() {
   return (
-    <div
-      className={`flex items-center justify-center text-sm p-5 text-gray-300 `}
-    >
+    <div className="flex items-center justify-center text-sm p-5 text-gray-300">
       &copy; {new Date().getFullYear()} Isaque de Sousa. Todos os direitos
       reservados.
     </div>
