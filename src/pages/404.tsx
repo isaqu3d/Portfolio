698a20e872dcd30633e8c70d6ffce7f0dfcb26e4
Error404 <isaque.error404@gmail.com>
2023-07-23 13:55:22 -0300

    refactor: comment className

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,21 +5,19 @@ import { Heading } from "../components/Heading";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col justify-center h-full">
+    <div className="flex h-full flex-col justify-center">
       <NextSeo
         title="Pagina não encontrada"
         description="Pagina não encontrada, error 404"
       />
-      <Heading className="no-underline">Não encontrado</Heading>
+      <Heading>Não encontrado</Heading> {/* className="no-underline" */}
       <p>A pagina que você está procurando não foi encontrada</p>
-      <div className="flex justify-center mt-8">
-        <span className="text-6xl px-6 py-2 font-sans font-semibold text-white transition duration-200 ease-in-out delay-300 skew-x-12 bg-purple-600 border-purple-700 rounded shadow-lg shadow-purple-600/50 hover:skew-y-6 hover:border-purple-800 text-white-100">
+      <div className="mt-8 flex justify-center">
+        <span className="text-white skew-x-12 rounded border-purple-700 bg-purple-600 px-6 py-2 font-sans text-6xl font-semibold text-white-100 shadow-lg shadow-purple-600/50 transition delay-300 duration-200 ease-in-out hover:skew-y-6 hover:border-purple-800">
           404
         </span>
       </div>
-
-      <div className="w-full h-[1px] bg-gray-600 my-8"></div>
-
+      <div className="my-8 h-[1px] w-full bg-gray-600"></div>
       <div className="flex justify-center">
         <ButtonLink href="/">Retorne ao inicio</ButtonLink>
       </div>
