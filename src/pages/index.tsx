3ab235388b58096c4926f615d06d08804718902d
Error404 <isaque.error404@gmail.com>
2023-07-23 14:00:57 -0300

    refactor: removed NextLink

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import NextLink from "next/link";
 import { BiChevronRight } from "react-icons/bi";
 
 import Bio from "../components/Bio";
@@ -57,17 +56,6 @@ export default function Home() {
           <ButtonLink href="/projects">
             Projetos <BiChevronRight />
           </ButtonLink>
-
-          <NextLink
-            href="/projects"
-            className={`${
-              theme
-                ? " bg-teal-400 text-white-100 hover:bg-teal-600 "
-                : " bg-teal-200 text-black hover:bg-teal-400"
-            } flex items-center gap-2 rounded-md py-2 px-4 font-medium transition-colors`}
-          >
-            Projetos
-          </NextLink>
         </div>
 
         <div>
