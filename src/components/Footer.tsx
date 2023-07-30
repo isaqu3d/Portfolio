f5b259097d060d86dc22a92c1f5aac4a7e0816d4
Error404 <isaque.error404@gmail.com>
2023-07-30 09:14:38 -0300

    refactor: removed tag span to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <span className="flex items-center justify-center text-sm p-5 text-gray-300">
+    <footer className="flex items-center justify-center p-5 text-sm text-gray-300">
       &copy; {new Date().getFullYear()} Isaque de Sousa. Todos os direitos
       reservados.
-    </span>
+    </footer>
   );
 }
