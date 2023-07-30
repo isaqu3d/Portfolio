b5eba31b867c9c1840254aecec7a8d8f42e34735
Error404 <isaque.error404@gmail.com>
2023-07-30 09:09:46 -0300

    refactor: add 'use client'

--- src/components/Motion.tsx
+++ src/components/Motion.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import { motion } from "framer-motion";
 export function Motion({ children }) {
   return (
     <motion.div
-      initial={{ y: 10, opacity: 0 }}
+      initial={{ y: 50, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: 20, opacity: 0 }}
-      transition={{ duration: 0.8 }}
+      transition={{ duration: 0.6, delay: 0.0 }}
     >
       {children}
     </motion.div>
