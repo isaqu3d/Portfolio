3f1e35cace7aa3f6cad54218dd4e979d1d8e75ae
Error404 <isaque.error404@gmail.com>
2023-07-30 09:14:05 -0300

    refactor: removed tag a

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,24 +1,25 @@
 import useThemeData from "@hook/useThemeData";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 type ButtonProps = {
   children: ReactNode;
-  className?: string;
   href: string;
 };
 
-export function ButtonLink({ children, className, href }: ButtonProps) {
+export function ButtonLink({ children, href }: ButtonProps) {
   const { theme } = useThemeData();
+
   return (
-    <a
+    <Link
       href={href}
       className={`${
         theme
           ? " bg-teal-400 text-white-100 hover:bg-teal-600 "
           : " bg-teal-200 text-black hover:bg-teal-400"
-      } ${className} flex items-center gap-2 rounded-md py-2 px-4 font-medium transition-colors`}
+      } flex items-center gap-2 rounded-md py-2 px-4 font-medium transition-colors`}
     >
       {children}
-    </a>
+    </Link>
   );
 }
