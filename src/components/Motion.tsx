6c89b54de2db0eeebde9cdde2955bc7a3825c0a9
Error404 <isaque.error404@gmail.com>
2023-07-30 20:28:36 -0300

    style: add new styles

--- src/components/Motion.tsx
+++ src/components/Motion.tsx
@@ -4,10 +4,10 @@ import { motion } from "framer-motion";
 export function Motion({ children }) {
   return (
     <motion.div
-      initial={{ y: 50, opacity: 0 }}
+      initial={{ y: 60, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: 20, opacity: 0 }}
-      transition={{ duration: 0.6, delay: 0.0 }}
+      transition={{ duration: 0.6, delay: 0.3 }}
     >
       {children}
     </motion.div>
