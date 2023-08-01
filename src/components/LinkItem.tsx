b8cbea95a6eb970251deeaeaadcf6c094c9137b8
Error404 <isaque.error404@gmail.com>
2023-08-01 20:39:06 -0300

    refactor: removed tag a

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -12,20 +12,17 @@ export function LinkItem({ href, children, target, ...props }: LinkItemProps) {
   const { theme } = useThemeData();
 
   return (
-    <NextLink href={href} passHref scroll={false}>
-      <a
-        href={href}
-        className={`${
-          theme
-            ? "text-teal-700  hover:decoration-teal-600"
-            : "text-teal-200  hover:decoration-pink-500"
-        } ml-2 flex w-36 items-center gap-2 rounded-md p-2 font-semibold  hover:bg-teal-400/20 hover:underline hover:decoration-1 hover:underline-offset-[6px]
-    `}
-        target={target}
-        {...props}
-      >
-        {children}
-      </a>
+    <NextLink
+      href={href}
+      scroll={false}
+      className={`${
+        theme
+          ? "text-teal-700  hover:decoration-teal-600"
+          : "text-teal-200  hover:decoration-pink-500"
+      } ml-2 flex w-36 items-center gap-2 rounded-md p-2 font-semibold hover:bg-teal-400/20 hover:underline hover:decoration-1 hover:underline-offset-[6px]
+`}
+    >
+      {children}
     </NextLink>
   );
 }
