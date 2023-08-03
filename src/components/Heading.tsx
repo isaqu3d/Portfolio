b68de3430ec5158c80519ed3ab6451a0399e783d
Error404 <isaque.error404@gmail.com>
2023-08-02 21:25:21 -0300

    refactor: removed className and types

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,20 +1,12 @@
-import useThemeData from "@hook/useThemeData";
 import { ReactNode } from "react";
 
 type HeadingProps = {
   children: ReactNode;
-  className?: string;
 };
 
-export function Heading({ className, children }: HeadingProps) {
-  const { theme } = useThemeData();
-
+export function Heading({ children }: HeadingProps) {
   return (
-    <h1
-      className={`${
-        theme ? "text-gray-600" : "text-white-100"
-      } mb-4 mt-3 font-sansMPlus text-xl font-bold underline decoration-gray-300 decoration-4 underline-offset-[6px] ${className}`}
-    >
+    <h1 className="mb-4 mt-3 font-sansMPlus text-xl font-bold underline decoration-gray-300 decoration-4 underline-offset-[6px]">
       {children}
     </h1>
   );
