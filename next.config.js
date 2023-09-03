2422e6859cd8ab4f75369e78e2af1e771bc81e70
Error404 <isaque.error404@gmail.com>
2023-09-03 11:44:07 -0300

    feat: add new domains

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["cdn.sanity.io"],
+    domains: [
+      "cdn.sanity.io",
+      "avatars.githubusercontent.com",
+      "ugc.production.linktr.ee",
+      "media.licdn.com",
+    ],
   },
 };
 
