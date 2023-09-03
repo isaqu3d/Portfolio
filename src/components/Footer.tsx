9e3465c20961d3af9d23191e6d91e473e6a01159
Error404 <isaque.error404@gmail.com>
2023-09-03 11:46:41 -0300

    feat: add new styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center p-5 text-sm text-gray-300">
-      &copy; {new Date().getFullYear()} Isaque de Sousa. Todos os direitos
-      reservados.
+    <footer>
+      <div className="mt-2 h-[1px] w-full bg-gray-600" />
+      <span className="flex items-center justify-center p-5 text-sm text-gray-300">
+        &copy; {new Date().getFullYear()} Isaque de Sousa. Todos os direitos
+        reservados.
+      </span>
     </footer>
   );
 }
