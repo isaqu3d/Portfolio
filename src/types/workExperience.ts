d0c1a580373ccf787dbb3dbcee9a2e6583533e1b
Error404 <isaque.error404@gmail.com>
2023-09-04 22:11:59 -0300

    feat: create types and add WorkExperienceProps

---
+++ src/types/workExperience.ts
@@ -0,0 +1,11 @@
+export type WorkExperienceProps = {
+  _id: string;
+  companyName: string;
+  companyUrl: string;
+  companyLogo: string;
+  role: string;
+  startDate: string;
+  endDate: string;
+  technologies: string[];
+  description: any;
+};
