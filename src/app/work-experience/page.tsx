b7666f2d5643dfb63deab9f6213eace1da7abf9a
Error404 <isaque.error404@gmail.com>
2023-09-04 22:03:38 -0300

    feat: create new page WorkExperiencePage and add Experience

---
+++ src/app/work-experience/page.tsx
@@ -0,0 +1,14 @@
+import { Experience } from "@components/Experience";
+import { groq } from "next-sanity";
+import client from "../../lib/sanityClient";
+
+export default async function WorkExperiencePage() {
+  const [experiences] = await client.fetch(groq`*[_type == "experiences"]`);
+
+  return (
+    <div className="max-w-4xl px-4">
+      {/* @ts-expect-error Server Component */}
+      <Experience experiences={experiences} />
+    </div>
+  );
+}
