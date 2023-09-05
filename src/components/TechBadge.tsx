056470104fa88e72cf61b404d11a3cb5d4a0bc97
Error404 <isaque.error404@gmail.com>
2023-09-04 22:05:52 -0300

    refactor: add framer-motion and types

--- src/components/TechBadge.tsx
+++ src/components/TechBadge.tsx
@@ -1,11 +1,18 @@
-type TechBadgeProps = {
+"use client";
+import { motion } from "framer-motion";
+import { ComponentProps } from "react";
+
+type TechBadgeProps = ComponentProps<typeof motion.span> & {
   name: string;
 };
 
-export function TechBadge({ name }: TechBadgeProps) {
+export function TechBadge({ name, ...props }: TechBadgeProps) {
   return (
-    <span className="rounded-lg bg-emerald-900/80 py-1 px-3 text-sm text-emerald-400">
+    <motion.span
+      className="hover: cursor-default rounded-lg bg-teal-600 py-1 px-3 text-sm text-white-100 transition-all hover:scale-105 hover:bg-teal-500"
+      {...props}
+    >
       {name}
-    </span>
+    </motion.span>
   );
 }
