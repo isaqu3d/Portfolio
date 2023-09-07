563326615f5dc16004e16d091703bb15fe9ddc5f
Error404 <isaque.error404@gmail.com>
2023-09-06 23:30:08 -0300

    style: add new styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <div className="mt-2 h-[1px] w-full bg-gray-600" />
+      <div className="mt-4 h-[1px] w-full bg-gray-600" />
       <span className="flex items-center justify-center p-5 text-sm text-gray-300">
         &copy; {new Date().getFullYear()} Isaque de Sousa. Todos os direitos
         reservados.
