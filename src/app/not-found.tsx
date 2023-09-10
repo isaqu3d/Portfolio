cb77f03aecafe0381e593e22d1ac77e384e0264a
Error404 <isaque.error404@gmail.com>
2023-09-10 19:38:41 -0300

    style: add new styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,15 +8,18 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <div className="flex h-full flex-col justify-center">
-      <Heading>Não encontrado</Heading> {/* className="no-underline" */}
-      <p>A pagina que você está procurando não foi encontrada</p>
+    <div className="flex h-full max-w-xl flex-col justify-center px-4">
+      <Heading>Não encontrado!</Heading>
+      <p className="text-xl">
+        Oops! A pagina que você está procurando não foi encontrada.
+      </p>
+
       <div className="mt-8 flex justify-center">
-        <span className="text-white skew-x-12 rounded border-purple-700 bg-purple-600 px-6 py-2 font-sans text-6xl font-semibold text-white-100 shadow-lg shadow-purple-600/50 transition delay-300 duration-200 ease-in-out hover:skew-y-6 hover:border-purple-800">
-          404
+        <span className="text-white rounded border-purple-700 bg-purple-600 px-6 py-2 font-sans text-6xl font-semibold text-white-100 shadow-lg shadow-purple-600/50 transition hover:skew-x-12  hover:border-purple-800">
+          Error 404
         </span>
       </div>
-      <div className="my-8 h-[1px] w-full bg-gray-600"></div>
+      <div className="my-8 h-[1px] w-full bg-gray-600" />
       <div className="flex justify-center">
         <ButtonLink href="/">Retorne ao inicio</ButtonLink>
       </div>
