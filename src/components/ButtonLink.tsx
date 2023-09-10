5cdffe7dc94e2a68f9253b7edba99aae1f922c33
Error404 <isaque.error404@gmail.com>
2023-09-10 19:40:04 -0300

    style: add new styles

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -2,9 +2,9 @@
 
 import useThemeData from "@hook/useThemeData";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { ComponentProps, ReactNode } from "react";
 
-type ButtonProps = {
+type ButtonProps = ComponentProps<"button"> & {
   children: ReactNode;
   href: string;
 };
@@ -13,15 +13,17 @@ export function ButtonLink({ children, href }: ButtonProps) {
   const { theme } = useThemeData();
 
   return (
-    <Link
-      href={href}
-      className={`${
-        theme
-          ? " bg-teal-400 text-white-100 hover:bg-teal-600 "
-          : " bg-teal-200 text-black hover:bg-teal-400"
-      } flex items-center gap-2 rounded-md py-2 px-4 font-medium transition-colors`}
-    >
-      {children}
-    </Link>
+    <div className="flex justify-center">
+      <Link
+        href={href}
+        className={`${
+          theme
+            ? " bg-teal-400 text-white-100 hover:bg-teal-600 "
+            : " bg-teal-200 text-black hover:bg-teal-400"
+        } my-2 flex items-center justify-center gap-2 rounded-md py-2 px-4 font-medium transition-colors`}
+      >
+        {children}
+      </Link>
+    </div>
   );
 }
