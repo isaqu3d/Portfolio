c07744426fdff715f0e8b02ca66b6df1b5f60dd9
Error404 <isaque.error404@gmail.com>
2023-09-11 19:08:14 -0300

    feat: create new component

---
+++ src/components/NextLinkColor.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import useThemeData from "@hook/useThemeData";
+import NextLink from "next/link";
+import { BiChevronRight } from "react-icons/bi";
+
+export function NextLinkColor() {
+  const { theme } = useThemeData();
+  return (
+    <div className="flex items-center">
+      <NextLink
+        href="/projects"
+        className={`${
+          theme
+            ? "text-teal-600 hover:underline"
+            : "text-pink-400 hover:underline"
+        } text-xs md:text-base`}
+      >
+        Projeto
+      </NextLink>
+
+      <span>
+        <BiChevronRight />
+      </span>
+    </div>
+  );
+}
