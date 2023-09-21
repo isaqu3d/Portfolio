c6d3638af1201f65c44b21c275fed5c605615355
Error404 <isaque.error404@gmail.com>
2023-09-21 00:46:58 -0300

    style: add new styles

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,11 +2,14 @@ import { ReactNode } from "react";
 
 type HeadingProps = {
   children: ReactNode;
+  className?: string;
 };
 
-export function Heading({ children }: HeadingProps) {
+export function Heading({ children, className }: HeadingProps) {
   return (
-    <h1 className="mb-4 mt-3 font-sansMPlus text-xl font-bold underline decoration-gray-300 decoration-4 underline-offset-[6px]">
+    <h1
+      className={`${className} mb-4 mt-3 py-2 font-sansMPlus text-2xl font-medium underline decoration-gray-300 decoration-4 underline-offset-[6px]`}
+    >
       {children}
     </h1>
   );
