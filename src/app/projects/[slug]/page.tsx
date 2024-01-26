ee766bc4dd3de767aa7697c81cc55c0fcf50b235
Isaque de Sousa <isaque.error404@gmail.com>
2024-01-26 16:22:35 -0300

    feat: add new style

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -62,9 +62,24 @@ export default async function Slug({ params: { slug } }) {
         <div className="flex flex-col items-center gap-2 md:flex-row md:items-start">
           <p>Tech:</p>
 
-          <div className="mt-2 flex flex-wrap justify-center gap-x-[6px] gap-y-4 md:mt-0 md:justify-start lg:max-w-[350px]">
-            {project.tech?.map((badge, i) => (
-              <TechBadge name={badge} key={`${i}-${badge}`} size="sm" />
+          <div className="mt-2 flex flex-wrap justify-center gap-x-[6px] gap-y-4 md:mt-0 md:justify-start lg:max-w-[550px]">
+            {project.technologies?.map((technology) => (
+              <li
+                key={technology._id}
+                className="relative flex items-center gap-2 overflow-hidden rounded-lg border border-border/50 bg-zinc-800 px-3 py-2 text-zinc-200 transition hover:scale-105 hover:bg-gray-600"
+              >
+                {technology.image ? (
+                  <Image
+                    src={urlFor(technology.image)?.url()}
+                    alt={technology.name ?? ""}
+                    width={16}
+                    height={16}
+                    quality={90}
+                  />
+                ) : null}
+                <p>{technology.name}</p>
+                <div className="via-10% absolute right-0 top-0 h-px w-80 bg-gradient-to-l from-transparent via-white-100/30 to-transparent" />
+              </li>
             ))}
           </div>
         </div>
