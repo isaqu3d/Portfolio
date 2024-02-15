d96993c750397a0ef1f4811a2402b8d86b5681ff
Isaque de Sousa <isaque.error404@gmail.com>
2024-02-15 09:45:14 -0300

    feat: add new type and create function

--- src/components/ButtonTheme.tsx
+++ src/components/ButtonTheme.tsx
@@ -1,5 +1,6 @@
 "use client";
 import useThemeData from "@/hook/useThemeData";
+import { MouseEventHandler } from "react";
 import { FiSun } from "react-icons/fi";
 import { RiMoonFill } from "react-icons/ri";
 
@@ -7,13 +8,18 @@ export function ButtonTheme() {
   const { theme, changeTheme } = useThemeData();
   const isDark = theme === "dark";
 
+  const handleClick: MouseEventHandler<HTMLButtonElement> = () => {
+    const newTheme = isDark ? "" : "dark";
+    changeTheme(newTheme);
+  };
+
   return (
     <button
-      onClick={changeTheme}
+      onClick={handleClick}
       aria-label="button changed dark mode and light mode"
       className={`${
         isDark
-          ? "animate-ButtonThemeFall text-white-100 bg-purple-700 hover:bg-purple-800"
+          ? "animate-ButtonThemeFall bg-purple-700 text-white-100 hover:bg-purple-800"
           : "animate-ButtonThemeFall2 bg-yellow-300 text-black hover:bg-yellow-500"
       } flex cursor-pointer items-center justify-center rounded-lg p-3`}
     >
