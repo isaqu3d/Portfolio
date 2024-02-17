9156e864957b0ab48c0cc5de37e8055415ef92e5
Isaque de Sousa <isaque.error404@gmail.com>
2024-02-17 17:18:52 -0300

    feat: removed use theme data and add new dark mode

--- src/app/projects/[slug]/loading.tsx
+++ src/app/projects/[slug]/loading.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { Skeleton } from "@/components/Skeleton";
-import useThemeData from "@/hook/useThemeData";
 
 export default function ProjectLoading() {
-  const { theme } = useThemeData();
-
   return (
     <div
       className="isolate
@@ -13,37 +10,13 @@ export default function ProjectLoading() {
    shadow-xl shadow-black/5
    before:border-t before:border-rose-100/10"
     >
-      <div
-        className={`${
-          theme ? "bg-slate-200" : "bg-zinc-800 "
-        } w-96 space-y-5 rounded-2xl p-4`}
-      >
-        <Skeleton
-          className={`${
-            theme ? "bg-slate-300" : "bg-zinc-700"
-          } h-24 rounded-lg`}
-        />
+      <div className="w-96 space-y-5 rounded-2xl bg-zinc-800 p-4 dark:bg-slate-200">
+        <Skeleton className="h-24 rounded-lg bg-zinc-700 dark:bg-slate-300" />
         <div className="space-y-3">
-          <Skeleton
-            className={`${
-              theme ? "bg-slate-300" : "bg-rose-100/10"
-            } h-3 w-3/5 rounded-lg`}
-          />
-          <Skeleton
-            className={`${
-              theme ? "bg-slate-300" : "bg-rose-100/20"
-            } h-3 w-4/5 rounded-lg`}
-          />
-          <Skeleton
-            className={`${
-              theme ? "bg-slate-300" : "bg-rose-100/20"
-            } h-3 w-2/5 rounded-lg`}
-          />
-          <Skeleton
-            className={`${
-              theme ? "bg-slate-300" : " bg-rose-100/10"
-            } h-3 w-3/5 rounded-lg`}
-          />
+          <Skeleton className="h-3 w-3/5 rounded-lg bg-rose-100/10 dark:bg-slate-300" />
+          <Skeleton className="h-3 w-3/5 rounded-lg bg-rose-100/10 dark:bg-slate-300" />
+          <Skeleton className="h-3 w-3/5 rounded-lg bg-rose-100/10 dark:bg-slate-300" />
+          <Skeleton className="h-3 w-3/5 rounded-lg bg-rose-100/10 dark:bg-slate-300" />
         </div>
       </div>
     </div>
