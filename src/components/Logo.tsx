82d7e3607c87423772c622d07f437056af408504
Isaque de Sousa <isaque.error404@gmail.com>
2024-02-17 23:25:36 -0300

    feat: add type and new styles

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,16 @@
+import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { RiCodeSSlashLine } from "react-icons/ri";
 
-export default function Logo() {
+export default function Logo({ className }: { className?: string }) {
   return (
     <Link href="/">
-      <span className="group inline-flex h-8 items-center p-3 font-sansMPlus text-lg font-semibold tracking-tighter text-white-100 dark:text-gray-600">
+      <span
+        className={cn(
+          "group inline-flex h-8 items-center p-3 font-sansMPlus text-lg font-semibold tracking-tighter text-white-100 dark:text-gray-600",
+          className,
+        )}
+      >
         <RiCodeSSlashLine className="transition-all group-hover:rotate-[30deg]" />
         <h1>Isaque de Sousa</h1>
       </span>
