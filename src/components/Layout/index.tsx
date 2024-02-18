0814e99ab479417f87fcab072941a97c4de2c11c
Isaque de Sousa <isaque.error404@gmail.com>
2024-02-17 23:28:33 -0300

    feat: add new styles

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,7 +7,7 @@ import Navbar from "../Navbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-gradient-to-br from-neutral-900 to-gray-900 text-white-100 dark:bg-gradient-to-br dark:from-white-100 dark:to-background dark:text-black">
+    <div className="bg-gradient-to-r from-neutral-900 to-gray-900 text-white-100 dark:bg-gradient-to-br dark:from-primary dark:to-primary dark:text-black">
       <Navbar />
 
       <main className="flex min-h-screen justify-center pt-[100px]">
