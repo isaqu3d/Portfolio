669f148688cd58fc1934cc3cc571d90f782ec468
Isaque de Sousa <isaque.error404@gmail.com>
2024-02-19 14:36:17 -0300

    feat: removed component and on click

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { MyDialog } from "@/components/CardModalSkill";
 import { GithubCalendar } from "@/components/GithubCalendar";
 import { Heading } from "@/components/Heading";
 import { Motion } from "@/components/Motion";
 import { SkillsCard } from "@/components/SkillsCard";
 import { SKILLS_CARD } from "@/utils/skills-card";
 import { SKILLS_TOOLS } from "@/utils/skills-tools";
-import { useState } from "react";
 
 /* export const metadata: Metadata = {
   title: "Habilidades",
@@ -15,24 +13,15 @@ import { useState } from "react";
     "Aqui estão as tecnologias que eu uso atualmente e alguns ferramentas",
 }; */
 
-function SkillCardRender({ icon: Icon, color, onClick }) {
+function SkillCardRender({ icon: Icon, color }) {
   return (
-    <SkillsCard onClick={onClick}>
+    <SkillsCard>
       <Icon size={90} color={color} />
     </SkillsCard>
   );
 }
 
 export default function Skills() {
-  const [modalId, setModalId] = useState<string | null>(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  console.log(modalId);
-
-  const foundModal = SKILLS_CARD.find((skill) => skill.id === modalId);
-
-  const handleOnClose = () => setIsOpen(false);
-
   return (
     <div className="max-w-xl px-4">
       <Motion>
@@ -44,38 +33,9 @@ export default function Skills() {
                 icon={skill.icon}
                 key={skill.id}
                 color={skill.color}
-                onClick={() => setModalId(skill.id)}
               />
             ))}
-            {/*  {modalId && Boolean(foundModal) ? (
-              <CardModalSkill
-                title={foundModal?.title ?? ""}
-                description={foundModal?.description ?? ""}
-                isOpenModal={!isOpen}
-                toggleModal={() => setIsOpen(!isOpen)}
-              />
-            ) : null} */}
-            {modalId && Boolean(foundModal) ? (
-              <MyDialog
-                title={foundModal?.title ?? ""}
-                description={foundModal?.description ?? ""}
-                icon={foundModal?.icon ?? ""}
-                isOpenModal={isOpen}
-                toggleModal={() => setIsOpen(false)}
-              />
-            ) : null}
-
-            {/*  {modalId && Boolean(foundModal) ? (
-              <MeuModal
-                title={foundModal?.title ?? ""}
-                description={foundModal?.description ?? ""}
-                icon={foundModal?.icon ?? ""}
-                isOpenModal={() => setIsOpen(true)}
-                onClose={() => setIsOpen(false)}
-              />
-            ) : null} */}
           </div>
-          <button onClick={() => setIsOpen(true)}>open</button>
         </div>
 
         <div>
@@ -86,7 +46,6 @@ export default function Skills() {
                 icon={skill.icon}
                 key={skill.id}
                 color={skill.color}
-                onClick={() => setModalId(skill.id)}
               />
             ))}
           </div>
--- src/components/SkillsCard.tsx
+++ src/components/SkillsCard.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-import useThemeData from "@/hook/useThemeData";
 import { ReactNode } from "react";
 
 type SkillsCardProps = {
   children: ReactNode;
-  onClick: () => void;
 };
 
-export function SkillsCard({ children, onClick }: SkillsCardProps) {
-  const { theme } = useThemeData();
-
+export function SkillsCard({ children }: SkillsCardProps) {
   return (
     <div
-      className={`${
-        theme
-          ? "border-2 border-teal-400 hover:border-teal-600"
-          : "border-2 border-pink-400 hover:border-pink-500"
-      } flex scale-100 justify-center rounded border-2 p-4 opacity-100 transition hover:scale-110 hover:opacity-100 sm:opacity-40`}
-      onClick={onClick}
+      className="
+        flex scale-100 justify-center rounded border-2 border-pink-400 p-4 opacity-100 transition hover:scale-110 hover:border-pink-500 hover:opacity-100 sm:opacity-40 dark:border-teal-400 dark:hover:border-teal-600"
     >
       {children}
     </div>
