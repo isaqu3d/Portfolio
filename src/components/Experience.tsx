13b1f66e54fc360cdb9f00913f252096d1b24a29
Isaque de Sousa <isaque.error404@gmail.com>
2024-02-19 13:21:04 -0300

    feat: add new name motion

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,7 +3,7 @@ import client from "../lib/sanityClient";
 import { WorkExperienceProps } from "../types/workExperience";
 import { ExperienceItem } from "./ExperienceItem";
 import { Heading } from "./Heading";
-import { MotionTitle } from "./Motion";
+import { MotionSlide } from "./Motion";
 
 type WorkExperience = {
   experience: WorkExperienceProps;
@@ -17,14 +17,14 @@ export async function Experience() {
   return (
     <>
       <section className="flex flex-col gap-10 py-0 md:gap-4 lg:flex-row lg:gap-16 lg:py-16">
-        <MotionTitle>
+        <MotionSlide>
           <div className="w-full lg:w-[250px]">
             <Heading>Experiência</Heading>
             <p className="text-gray-500">
               Todas minhas experiências profissionais detalhadas para você!
             </p>
           </div>
-        </MotionTitle>
+        </MotionSlide>
 
         <div className="flex flex-col gap-4">
           {experiences?.map((experience: WorkExperience) => (
