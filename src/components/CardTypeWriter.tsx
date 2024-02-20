46d905c80e92926e69182b1ba23a0aee081f4fb1
Isaque de Sousa <isaque.error404@gmail.com>
2024-02-20 00:58:34 -0300

    feat: add new styles

--- src/components/CardTypeWriter.tsx
+++ src/components/CardTypeWriter.tsx
@@ -2,7 +2,7 @@ import { Typewriter } from "./TypeWriter";
 export function CardTypeWriter() {
   return (
     <>
-      <div className="mb-4 flex h-12 items-center justify-center rounded-lg bg-gray-600 bg-opacity-40  p-1 text-center text-white-100 shadow-xl dark:bg-white-100 dark:text-black">
+      <div className="mb-4 flex h-12 items-center justify-center rounded-lg bg-gray-600  bg-opacity-40 p-1 text-center text-white-100 shadow-md dark:bg-white-100 dark:text-black">
         <Typewriter />
       </div>
     </>
