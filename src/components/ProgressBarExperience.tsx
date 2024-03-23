f62d0c77c0ea5b1724ab8a357432049aed326930
Isaque de Sousa <isaque.error404@gmail.com>
2024-03-23 12:30:47 -0300

    style: add new color

--- src/components/ProgressBarExperience.tsx
+++ src/components/ProgressBarExperience.tsx
@@ -1,5 +1,5 @@
 "use client";
 
 export function ProgressBarExperience() {
-  return <div className=" h-full w-[2px] bg-teal-600 dark:bg-pink-800" />;
+  return <div className=" h-full w-[2px] bg-zinc-500" />;
 }
