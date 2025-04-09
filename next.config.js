7b35bf6baf2283864e9eddee1e1e7221dd520e53
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-09 11:46:59 -0300

    fix: add webpack alias for source directory

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
   images: {
     remotePatterns: [
       {
