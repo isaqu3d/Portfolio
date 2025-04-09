dca0134cd089616c5e7baef40cdc5ebe78262d76
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-09 10:06:14 -0300

    refactor: refactor CardTypeWriter component

--- src/components/TypeWriter.tsx
+++ src/components/TypeWriter.tsx
@@ -1,21 +1,25 @@
+"use client";
+
 import TypewriterComponent from "typewriter-effect";
 
 export function Typewriter() {
   return (
-    <TypewriterComponent
-      options={{
-        strings: [
-          "< Olá, Seja bem-vindo ao meu portfolio",
-          "< Entusiasta da tecnologia",
-          "< Sou Desenvolvedor Web e Mobile",
-          "< Front-end",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 15,
-        delay: 45,
-        cursor: " />",
-      }}
-    />
+    <div className="mb-4 flex h-12 items-center justify-center rounded-lg bg-gray-600  bg-opacity-40 p-1 text-center text-white-100 shadow-md dark:bg-white-100 dark:text-black">
+      <TypewriterComponent
+        options={{
+          strings: [
+            "< Olá, Seja bem-vindo ao meu portfolio",
+            "< Entusiasta da tecnologia",
+            "< Sou Desenvolvedor Web e Mobile",
+            "< Front-end",
+          ],
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 15,
+          delay: 45,
+          cursor: " />",
+        }}
+      />
+    </div>
   );
 }
