82d7c683d6212b57dc6f320efdfe09746ac239d3
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-08 22:59:12 -0300

    refactor: refactor Heading component to use cn utility function

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/utils/cn";
 import { ReactNode } from "react";
 
 type HeadingProps = {
@@ -8,7 +9,10 @@ type HeadingProps = {
 export function Heading({ children, className }: HeadingProps) {
   return (
     <h1
-      className={`${className} mb-4 mt-3 py-2 font-sansMPlus text-2xl font-medium underline decoration-gray-300 decoration-4 underline-offset-[6px]`}
+      className={cn(
+        "mb-4 mt-3 py-2 font-sansMPlus text-2xl font-medium underline decoration-gray-300 decoration-4 underline-offset-[6px]",
+        className,
+      )}
     >
       {children}
     </h1>
