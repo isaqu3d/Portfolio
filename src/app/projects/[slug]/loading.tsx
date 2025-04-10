16f37432717b5814b0013b9de7358f611c604538
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-10 11:51:50 -0300

    chore: update import path for Skeleton component

--- src/app/projects/[slug]/loading.tsx
+++ src/app/projects/[slug]/loading.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Skeleton } from "../../../components/skeleton";
+import { Skeleton } from "@/components/skeleton";
 
 export default function ProjectLoading() {
   return (
