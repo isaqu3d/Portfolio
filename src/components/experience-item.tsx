42bac7d9c5e665ccc4d789ae59b41912bd12a71b
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-14 18:50:04 -0300

    refactor: refactor ExperienceItem component to use translations

--- src/components/experience-item.tsx
+++ src/components/experience-item.tsx
@@ -1,26 +1,44 @@
+import { getTranslations } from "@/lib/get-translations";
 import formatDate from "@/utils/format-date";
 import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 import { urlFor } from "../lib/urlSanity";
-import { WorkExperienceProps } from "../types/workExperience";
 import { Button } from "./button";
 import { MotionSection } from "./motion";
 import { ProgressBarExperience } from "./progressbar-experience";
 import { Technology } from "./technology";
 
-type ExperienceItemProps = {
-  experience: WorkExperienceProps;
-};
+export async function ExperienceItem({
+  params,
+}: {
+  params: { locale: string };
+}) {
+  const { locale } = params;
 
-export async function ExperienceItem({ experience }: ExperienceItemProps) {
+  const translations = await getTranslations(locale);
+
+  if (!translations) {
+    return <p>Erro ao carregar traduções para o idioma: {locale}</p>;
+  }
+
+  const { experiences } = translations;
+
+  const description = experiences?.description;
+
+  const finalDescription = Array.isArray(description)
+    ? description
+    : description
+      ? [description]
+      : [];
+
   return (
     <MotionSection>
       <section className="grid grid-cols-[40px,1fr] gap-4 md:gap-10">
         <div className="flex flex-col items-center gap-4">
           <div className="rounded-full border border-gray-500 p-[2px]">
             <Image
-              src={urlFor(experience.companyLogo)?.url()}
-              alt={experience.companyName}
+              src={urlFor(experiences?.companyLogo ?? "")?.url()}
+              alt={translations.experiences?.companyName ?? ""}
               width={50}
               height={50}
               quality={90}
@@ -35,25 +53,27 @@ export async function ExperienceItem({ experience }: ExperienceItemProps) {
           <div className="flex flex-col gap-2">
             <Button
               variant="link"
-              href={experience.companyUrl}
+              href={translations.experiences?.companyUrl}
               target="_blank"
               className="lg:text-md w-max text-base"
             >
-              <span>@</span> {experience.companyName}
+              <span>@</span> {translations.experiences?.companyName}
             </Button>
             <h2 className="font-medium text-gray-100 dark:text-gray-700">
-              {experience.role}
+              {translations.experiences?.role}
             </h2>
             <span className="text-gray-300 dark:text-gray-400">
-              {formatDate(experience.startDate)} -{" "}
-              {experience.endDate
-                ? formatDate(experience.endDate)
+              {formatDate(experiences?.startDate ?? "")} -{" "}
+              {experiences?.endDate
+                ? formatDate(experiences?.endDate)
                 : "No momento"}
             </span>
 
-            <article className="prose text-gray-500">
-              <PortableText value={experience.description} />
-            </article>
+            {finalDescription.length > 0 && (
+              <article className="prose text-gray-500">
+                <PortableText value={finalDescription} />
+              </article>
+            )}
           </div>
 
           <div>
@@ -62,7 +82,7 @@ export async function ExperienceItem({ experience }: ExperienceItemProps) {
             </p>
 
             <div className="mb-8 flex flex-wrap gap-x-2 gap-y-4 lg:max-w-[350px]">
-              {experience.technologies.map((technology) => (
+              {experiences?.technologies.map((technology) => (
                 <Technology key={technology._id}>
                   {technology.image ? (
                     <Image
