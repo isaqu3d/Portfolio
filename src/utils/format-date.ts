863d000422e7eb5954a754fc5280de181ea4e03f
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-14 21:17:47 -0300

    refactor: update formatDate function to support multiple locales

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,14 +1,24 @@
 import { format } from "date-fns";
-import { ptBR } from "date-fns/locale";
+import { enUS, ptBR } from "date-fns/locale";
 
 const capitalize = (string: string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-export default function formatDate(date: string) {
+export default function formatDate(date: string, locale: string) {
+  let selectedLocale: Locale;
+
+  if (locale === "pt") {
+    selectedLocale = ptBR;
+  } else if (locale === "en") {
+    selectedLocale = enUS;
+  } else {
+    selectedLocale = enUS;
+  }
+
   return capitalize(
     format(new Date(date), "MMM yyyy", {
-      locale: ptBR,
+      locale: selectedLocale,
     }),
   );
 }
