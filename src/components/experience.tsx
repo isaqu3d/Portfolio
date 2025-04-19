a7cfcffb94d7b154a8073759a119f67137eaa117
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-19 18:20:20 -0300

    refactor: add local translations for experiences

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,3 +1,4 @@
+import { getLocalTranslations } from "@/lib/get-local-translations";
 import { getTranslations } from "@/lib/get-translations";
 import { ExperienceItem } from "./experience-item";
 import { Heading } from "./heading";
@@ -8,15 +9,15 @@ export async function Experience({ params }: { params: { locale: string } }) {
 
   const translations = await getTranslations(locale);
 
+  const local = getLocalTranslations(locale);
+
   return (
     <>
       <section className="flex flex-col gap-10 py-0 md:gap-4 lg:flex-row lg:gap-16 lg:py-16">
         <MotionSlide>
           <div className="w-full lg:w-[250px]">
-            <Heading>Experiências</Heading>
-            <p className="text-gray-500">
-              Todas minhas experiências profissionais detalhadas para você!
-            </p>
+            <Heading>{local.experiences.title}</Heading>
+            <p className="text-gray-500">{local.experiences.description}</p>
           </div>
         </MotionSlide>
 
