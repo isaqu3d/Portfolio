941e5a080fac07e484acd64dcc115bcad812866a
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-21 16:48:52 -0300

    refactor: refactor Layout component to include locale prop

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import { Command } from "@/components/Command/command";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({
+  children,
+  locale,
+}: {
+  children: React.ReactNode;
+  locale: string;
+}) {
   return (
     <div className="bg-gradient-to-r from-neutral-900 to-gray-900 text-white-100 dark:bg-gradient-to-br dark:from-primary dark:to-primary dark:text-black">
-      <Navbar />
+      <Navbar locale={locale} />
 
       <main className="flex min-h-screen justify-center pt-[100px]">
-        <div className="hidden lg:flex">
-          <Command />
-        </div>
         {children}
       </main>
 
