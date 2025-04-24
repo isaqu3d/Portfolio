55b337ab1d0f656e6b499f9e9ccd11a9a3786a7f
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-24 00:49:55 -0300

    refactor: refactor typewriter component to use localized translations

--- src/components/typewriter.tsx
+++ src/components/typewriter.tsx
@@ -1,18 +1,26 @@
 "use client";
 
+import { getLocalTranslations } from "@/lib/get-local-translations";
+import { useLocale, useTranslations } from "next-intl";
 import TypewriterComponent from "typewriter-effect";
 
 export function Typewriter() {
+  const locale = useLocale();
+  const translations = getLocalTranslations(locale);
+  const navbar = translations;
+  const t = useTranslations();
+
   return (
     <div className="mb-4 flex h-12 items-center justify-center rounded-lg bg-gray-600  bg-opacity-40 p-1 text-center text-white-100 shadow-md dark:bg-white-100 dark:text-black">
       <TypewriterComponent
         options={{
           strings: [
-            "< Olá, Seja bem-vindo ao meu portfolio",
-            "< Entusiasta da tecnologia",
-            "< Sou Desenvolvedor Web e Mobile",
-            "< Front-end",
+            t("typeWriter.welcomeMessage"),
+            t("typeWriter.techEnthusiast"),
+            t("typeWriter.developer"),
+            t("typeWriter.frontend"),
           ],
+
           autoStart: true,
           loop: true,
           deleteSpeed: 15,
