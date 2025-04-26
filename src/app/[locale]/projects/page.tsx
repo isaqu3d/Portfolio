7569ed6d76c6af086b7ef0213651eb0d3d15de46
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-26 20:17:10 -0300

    refactor: project page component

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,41 +1,19 @@
 "use client";
 
-import Image from "next/image";
-import NextLink from "next/link";
-
-import { Translations } from "@/@types/types";
 import { Heading } from "@/components/heading";
 import { MotionSlide } from "@/components/motion";
 import { getLocalTranslations } from "@/lib/get-local-translations";
-import { getTranslations } from "@/lib/get-translations";
-import { urlFor } from "@/lib/urlSanity";
-import ProjectLoading from "./[slug]/loading";
-
-import { useQuery } from "@tanstack/react-query";
+import ProjectLoading from "./components/loading";
+import { ProjectItem } from "./components/project-item";
+import { useProjects } from "./hooks/use-projects";
 
 export default function Projects({ params }: { params: { locale: string } }) {
+  const { data: translations, isLoading, error } = useProjects();
   const { locale } = params;
   const local = getLocalTranslations(locale);
 
-  const {
-    data: translations,
-    isLoading,
-    error,
-  } = useQuery<Translations | null>({
-    queryKey: ["translations", locale],
-    queryFn: async () => {
-      const res = await getTranslations(locale);
-      return res;
-    },
-  });
-
-  if (isLoading) {
-    return <ProjectLoading />;
-  }
-
-  if (error || !translations) {
-    return <p>Erro ao carregar projetos.</p>;
-  }
+  if (isLoading) return <ProjectLoading />;
+  if (error || !translations) return <p>Erro ao carregar projetos.</p>;
 
   const { projects } = translations;
 
@@ -50,24 +28,7 @@ export default function Projects({ params }: { params: { locale: string } }) {
 
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
           {projects.map((project) => (
-            <div className="w-full text-center" key={project._key}>
-              <NextLink href={`projects/${project.slug.current}`}>
-                <Image
-                  src={urlFor(project.thumbnail)
-                    .width(720)
-                    .height(420)
-                    .quality(90)
-                    .url()}
-                  alt={project.name}
-                  width={720}
-                  height={420}
-                  quality={90}
-                  className="rounded-lg shadow-md shadow-gray-600 transition-all hover:scale-110"
-                />
-                <h1 className="mt-2 text-xl">{project.name}</h1>
-                <p className="text-sm">{project.description}</p>
-              </NextLink>
-            </div>
+            <ProjectItem key={project._key} project={project} />
           ))}
         </div>
       </MotionSlide>
