b89aa317e3332f7d86c45cb16fb2551eb4421142
Isaque de Sousa <isaque.error404@gmail.com>
2025-11-22 11:23:17 -0300

    feat: add Technology and Typewriter components for enhanced UI interactivity

---
+++ src/components/shared/typewriter.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import TypewriterComponent from "typewriter-effect";
+
+export function Typewriter() {
+  const t = useTranslations();
+
+  return (
+    <div className="mb-4 flex h-12 items-center justify-center rounded-lg bg-gray-600  bg-opacity-40 p-1 text-center text-white-100 shadow-md dark:bg-white-100 dark:text-black">
+      <TypewriterComponent
+        options={{
+          strings: [
+            `< ${t("typeWriter.welcomeMessage")}`,
+            `< ${t("typeWriter.techEnthusiast")}`,
+            `< ${t("typeWriter.developer")}`,
+            `< ${t("typeWriter.fullstack")} >`,
+          ],
+
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 15,
+          delay: 45,
+          cursor: " />",
+        }}
+      />
+    </div>
+  );
+}
